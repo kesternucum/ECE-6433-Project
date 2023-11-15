499175dcb3216c616c6b1429caf511cb40383bbc
Ricky271828 <rk972@msstate.edu>
2023-11-15 17:31:13 -0600

    Modify the rand code

--- randgen.m
+++ randgen.m
@@ -1,9 +1,19 @@
 clear; close all; clc;
 
+% LFM param
+a =3*10^-6; %Min
+b = 3*10^-3;%Max
+tau_LFM = (b-a).*rand() + a;
 
-c = 3*10^8;%Speed of light
+a =10^6; %Min
+b = 4*10^8;%Max
+B_LFM = (b-a).*rand() + a; %Bandwidth
+TB = (B_LFM * tau_LFM); %At least more than 40
+F_LFM = 10*10^9;%10GHz
 
-F = rand()*10^7;%Frequency
-lambdha = c/F;%Wavelength
-tau = rand()*10^-6;%Pulsewidth
-B = 1/tau; %Bandwidth
+%Sinewave
+tau_sin = rand()*10^-4;%Pulsewidth
+F_sine = 10^6;%10MHz
+timeperiod = 0:1/F_sine:5*10^-3; %Do not exceed more than 10000 row
+
+%Phase
